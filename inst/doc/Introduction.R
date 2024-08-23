be2bc335f39630891eebb4b1e25056bd285ca3ee
Christos Adam <econp266@econ.soc.uoc.gr>
2024-08-23 11:20:02 +0000

    version 0.1.0

---
+++ inst/doc/Introduction.R
@@ -0,0 +1,30 @@
+## ----setup, include=FALSE-----------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE, eval=FALSE)
+
+## ----Figure1, eval=TRUE, echo=FALSE, out.width = '100%', fig.align = "center"----
+knitr::include_graphics("fig1.jpg")
+
+## ----Figure2, eval=TRUE, echo=FALSE, out.width = '50%', fig.align = "center"----
+knitr::include_graphics("fig2.jpg")
+
+## ----Figure3, eval=TRUE, echo=FALSE, out.width = '100%', fig.align = "center"----
+knitr::include_graphics("fig3.jpg")
+
+## ----Figure4, eval=TRUE, echo=FALSE, out.width = '60%', fig.align = "center"----
+knitr::include_graphics("fig4.jpg")
+
+## ----Figure5, eval=TRUE, echo=FALSE, out.width = '75%', fig.align = "center"----
+knitr::include_graphics("fig5.jpg")
+
+## ----Figure6, eval=TRUE, echo=FALSE, out.width = '75%', fig.align = "center"----
+knitr::include_graphics("fig6.jpg")
+
+## ----Figure7, eval=TRUE, out.width = '75%', fig.align = "center"--------------
+knitr::include_graphics("sa.jpg")
+
+## ----Figure8, eval=TRUE, echo=FALSE, out.width = '75%', fig.align = "center"----
+knitr::include_graphics("sku.jpg")
+
+## ----Figure9, eval=TRUE, echo=FALSE, out.width = '75%', fig.align = "center"----
+knitr::include_graphics("rao.jpg")
+
